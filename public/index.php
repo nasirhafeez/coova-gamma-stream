7030fe8adc8a9f3fc84ebb6e516de8a21b6dc6ca
Nasir Hafeez <nasirhafeez2002@gmail.com>
2022-12-17 08:20:12 +0500

    initial setup

--- public/index.php
+++ public/index.php
@@ -3,12 +3,17 @@
 require 'header.php';
 include 'config.php';
 
-$_SESSION["id"] = $_GET['id'];
-$_SESSION["ap"] = $_GET['ap'];
+$_SESSION["mac"] = $_GET["mac"];
+$_SESSION["challenge"] = $_GET["challenge"];
+$_SESSION["uamip"] = $_GET["uamip"];
+$_SESSION["uamport"] = $_GET["uamport"];
+$_SESSION["userurl"] = $_GET["userurl"];
 $_SESSION["user_type"] = "new";
 
+$res = $_GET["res"];
+
 # Checking DB to see if user exists or not.
-$result = mysqli_query($con, "SELECT * FROM `$table_name` WHERE mac='$_SESSION[id]'");
+$result = mysqli_query($con, "SELECT * FROM `$table_name` WHERE mac='$_SESSION[mac]'");
 
 if ($result->num_rows >= 1) {
   $row = mysqli_fetch_array($result);
@@ -30,20 +35,25 @@ if ($result->num_rows >= 1) {
   <title>
     <?php echo htmlspecialchars($business_name); ?> WiFi</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-  <link rel="stylesheet" href="../assets/styles/bulma.min.css" />
-  <link rel="stylesheet" href="../vendor/fortawesome/font-awesome/css/all.css" />
-  <link rel="icon" type="image/png" href="../assets/images/favicomatic/favicon-32x32.png" sizes="32x32" />
-  <link rel="icon" type="image/png" href="../assets/images/favicomatic/favicon-16x16.png" sizes="16x16" />
-  <link rel="stylesheet" href="../assets/styles/style.css" />
+  <link rel="stylesheet" href="assets/styles/bulma.min.css" />
+  <link rel="stylesheet" href="vendor/fortawesome/font-awesome/css/all.css" />
+  <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-32x32.png" sizes="32x32" />
+  <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-16x16.png" sizes="16x16" />
+  <link rel="stylesheet" href="assets/styles/style.css" />
 </head>
 
 <body>
+
+<?php
+if ($res === "notyet") {
+?>
+
   <div class="page">
 
     <div class="head">
       <br>
       <figure id="logo">
-        <img src="../assets/images/logo.png">
+        <img src="assets/images/logo.png">
       </figure>
     </div>
 
@@ -97,5 +107,19 @@ if ($result->num_rows >= 1) {
       </section>
     </div>
   </div>
+<?php
+} else if ($res === "success") {
+  header("Location: $redirect_url");
+} else if ($res === "failed") {
+  echo "<h2>Sorry, failed to authenticate</h2>";
+} else if ($res === "logoff") {
+  echo "<h2>Logging off...</h2>";
+} else if ($res === "already") {
+  header("Location: $redirect_url");
+} else {
+  echo "<h2>Error: Permission Denied</h2>";
+}
+?>
+
 </body>
 </html>
